dcd28b5577ab2c86d78e40828fd8a5f9050f1ac8
Shakthi19 <shakthi2619@gmail.com>
2023-08-19 23:50:50 +0530

    Add files via upload

---
+++ Implementation of Polymorphism/Shape/Main.java
@@ -0,0 +1,78 @@
+interface Shape {
+    double calculateArea();
+    double calculatePerimeter();
+}
+
+class Circle implements Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return 2 * Math.PI * radius;
+    }
+}
+
+class Rectangle implements Shape {
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public double calculateArea() {
+        return width * height;
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return 2 * (width + height);
+    }
+}
+
+class Triangle implements Shape {
+    private double base;
+    private double height;
+
+    public Triangle(double base, double height) {
+        this.base = base;
+        this.height = height;
+    }
+
+    @Override
+    public double calculateArea() {
+        return 0.5 * base * height;
+    }
+
+    @Override
+    public double calculatePerimeter() {
+                return 0.0;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Shape[] shapes = new Shape[3];
+        shapes[0] = new Circle(5.0);
+        shapes[1] = new Rectangle(4.0, 6.0);
+        shapes[2] = new Triangle(3.0, 4.0);
+
+        
+        for (Shape shape : shapes) {
+            System.out.println("Area: " + shape.calculateArea());
+            System.out.println("Perimeter: " + shape.calculatePerimeter());
+            System.out.println("-------------");
+        }
+    }
+}
