a6d1afeab89dbc560c3fa8711376b593a8862ca9
Shakthi19 <shakthi2619@gmail.com>
2023-09-11 22:38:45 +0530

    Add files via upload

---
+++ FRONTEND/TYPESCRIPT/Assignment4/Assignment4Main.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+var Assignment4_1 = require("./Assignment4");
+var Assignment4subclass_1 = require("./Assignment4subclass");
+var emp = new Assignment4_1.Employee("Sri");
+emp.salary();
+var dev = new Assignment4subclass_1.Developer("Shakthi");
+dev.salary();
---
+++ FRONTEND/TYPESCRIPT/Assignment4/Assignment4Main.ts
@@ -0,0 +1,9 @@
+import { Employee } from './Assignment4';
+import {Developer} from './Assignment4subclass';
+
+let emp=new Employee("Sri");
+emp.salary();
+let dev=new Developer("Shakthi");
+dev.salary();
+
+
