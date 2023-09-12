211fd68ebcb97e8d2fdfb9cd5b8a68e44f7a1f5f
Shakthi19 <shakthi2619@gmail.com>
2023-09-13 00:48:43 +0530

    Add files via upload

---
+++ FRONTEND/ANGULAR/Assignment 2/employee.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-employee',
+    templateUrl:'./employee.html',
+    styleUrls:['./employee.css']
+})
+
+export class EmployeeComponent{}
